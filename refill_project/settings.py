1d3427c63a9a0c93ed875d8fe07cb7358bd0af16
Raiyan <69578720+FarooquiRaiyan@users.noreply.github.com>
2026-01-08 13:17:03 +0530

    Update settings.py

--- refill_project/settings.py
+++ refill_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ks%wm^o=8ql@d^onr4r#701vhc5+muj)2pbv%rwt)bfxfkn8eg'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
